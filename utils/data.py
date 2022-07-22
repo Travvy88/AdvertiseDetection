508584bd634c0dd25b33f8fd8ba784b3d042d2af
Nikita Shevtsov <youngtravisbeatz@gmail.com>
2022-07-22 14:22:56 +0300

    check_coco_dataset added to utils.data

--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
-import os 
+import os
+import cv2
+from matplotlib import pyplot as plt
+import json
 
 def change_to_one_class(path, label='logo'):
     # сделать в xml один общий класс {label}
@@ -30,3 +33,21 @@ def add_path_to_xml(path):
     else:
         paths[0].text = path_to_jpg
     tree.write(path)
+
+
+def check_coco_dataset(json_path, idx):
+    # выводит сэмп из фото по аннотации
+    with open(json_path) as json_file:
+        data = json.load(json_file)
+
+    anno = data['annotations'][idx]
+    bbox = anno['bbox']
+    image = data['images'][anno['image_id']]
+    file_name = image['file_name']
+
+    img = cv2.imread('openlogo_coco/JPEGImages/' + file_name)
+    pt1 = (bbox[0], bbox[1])
+    pt2 = (bbox[0] + bbox[2], bbox[1] + bbox[3])
+
+    img = cv2.rectangle(img, pt1, pt2, (255, 0, 0), 3)
+    plt.imshow(img)
